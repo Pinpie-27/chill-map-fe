cee827fa1e3c39998c7b6905be0a95680bbaf51a
Pinpie-27 <hnnguyen0327@gmail.com>
2025-07-19 01:29:55 +0700

    first commit: build structure

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import "../app/App.tsx";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-pink-500 text-3xl">Chill Map</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import App from "./app/App.tsx";
+import ReactDOM from "react-dom/client";
+import React from "react";
+import "../src/styles/index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
